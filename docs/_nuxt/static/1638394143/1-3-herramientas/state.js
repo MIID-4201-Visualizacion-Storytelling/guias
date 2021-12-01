617fd090b410381d76958285f557bd59a56650f8
Juan Camilo González <j.gonzalezj@uniandes.edu.co>
2021-12-01 21:42:34 +0000

    🐛 definir base para que funcionen los enlaces

--- docs/_nuxt/static/1638394143/1-3-herramientas/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1638394143",layout:"default",error:a,serverRendered:true,routePath:"\u002F1-3-herramientas",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"f02809ac"}}}}(null));
