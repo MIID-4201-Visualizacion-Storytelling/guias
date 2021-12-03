b3e32472cd2d3b0ec22712d395dfaf6f455a8bc5
1cgonza <1cgonza@users.noreply.github.com>
2021-12-03 03:32:35 +0000

    deploy: acec2ae8402e569275b9ef4b34a708489f017c82

--- _nuxt/static/1638497944/2-5-interaccion/payload.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("/2-5-interaccion", {data:[{page:{slug:"2-5-interaccion",description:"...",title:"Interacción",toc:[],body:{type:"root",children:[]},dir:"\u002F",path:"\u002F2-5-interaccion",extension:".md",createdAt:"2021-12-03T02:18:12.368Z",updatedAt:"2021-12-03T02:18:12.372Z"}}],fetch:{},mutations:void 0});
---
+++ _nuxt/static/1638502354/3-1-extraer-limpiar/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fguias\u002F_nuxt\u002Fstatic\u002F1638502354",layout:"default",error:a,serverRendered:true,routePath:"\u002F3-1-extraer-limpiar",config:{_app:{basePath:"\u002Fguias\u002F",assetsPath:"\u002Fguias\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"6ca68e1e"}}}}(null));
