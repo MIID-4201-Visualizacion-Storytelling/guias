2817c6b5df725650b77b27c313591d12df64eaa7
1cgonza <1cgonza@users.noreply.github.com>
2021-12-03 02:19:05 +0000

    deploy: f3bcdc0cf61419ca965c78fc409a3f8b0a92d601

--- _nuxt/static/1638480485/1-1-formas-de-narrar/payload.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("/1-1-formas-de-narrar", (function(a){return {data:[{page:{slug:"1-1-formas-de-narrar",description:"...",title:"Formas de Narrar",toc:[],body:{type:"root",children:[]},dir:"\u002F",path:"\u002F1-1-formas-de-narrar",extension:".md",createdAt:a,updatedAt:a}}],fetch:{},mutations:void 0}}("2021-12-02T21:27:19.600Z")));
---
+++ _nuxt/static/1638497944/1-5-google-data-studio/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/1-5-google-data-studio", (function(a){return {data:[{page:{slug:"1-5-google-data-studio",description:"...",title:"Google Data Studio",toc:[],body:{type:"root",children:[]},dir:"\u002F",path:"\u002F1-5-google-data-studio",extension:".md",createdAt:a,updatedAt:a}}],fetch:{},mutations:void 0}}("2021-12-03T02:18:12.368Z")));
