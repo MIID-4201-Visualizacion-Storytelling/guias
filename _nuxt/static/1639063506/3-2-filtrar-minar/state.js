4a4d0b546fab263f4829a98687fd397579e7a471
1cgonza <1cgonza@users.noreply.github.com>
2021-12-09 15:31:06 +0000

    deploy: 096b95c39ef3059544b953409893ecacc328b980

--- _nuxt/static/1639063506/3-2-filtrar-minar/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fguias\u002F_nuxt\u002Fstatic\u002F1639063506",layout:"default",error:a,serverRendered:true,routePath:"\u002F3-2-filtrar-minar",config:{_app:{basePath:"\u002Fguias\u002F",assetsPath:"\u002Fguias\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"f879b685"}}}}(null));
---
+++ _nuxt/static/1639063865/5-1-plantillas-web/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fguias\u002F_nuxt\u002Fstatic\u002F1639063865",layout:"default",error:a,serverRendered:true,routePath:"\u002F5-1-plantillas-web",config:{_app:{basePath:"\u002Fguias\u002F",assetsPath:"\u002Fguias\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"070f15b6"}}}}(null));
