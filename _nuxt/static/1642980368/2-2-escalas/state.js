51623e28ca979872cf4fd5885e4b2c7b430911fc
1cgonza <1cgonza@users.noreply.github.com>
2022-01-23 23:26:10 +0000

    deploy: 66c4d93431dbbf20ddd6037482bdaef1d243374d

---
+++ _nuxt/static/1642980368/2-2-escalas/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fguias\u002F_nuxt\u002Fstatic\u002F1642980368",layout:"default",error:a,serverRendered:true,routePath:"\u002F2-2-escalas",config:{_app:{basePath:"\u002Fguias\u002F",assetsPath:"\u002Fguias\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"eb381915"}}}}(null));
