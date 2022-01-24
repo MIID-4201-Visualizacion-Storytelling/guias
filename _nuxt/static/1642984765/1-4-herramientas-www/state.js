340f538e68b4352a8543875ce58facae7774ff1a
1cgonza <1cgonza@users.noreply.github.com>
2022-01-24 00:49:56 +0000

    deploy: adb785ab5f1fc230faa20528bcb1127e2cba3ed8

--- _nuxt/static/1642984765/1-4-herramientas-www/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fguias\u002F_nuxt\u002Fstatic\u002F1642984765",layout:"default",error:a,serverRendered:true,routePath:"\u002F1-4-herramientas-www",config:{_app:{basePath:"\u002Fguias\u002F",assetsPath:"\u002Fguias\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"f52051f8"}}}}(null));
---
+++ _nuxt/static/1642985394/5-1-plantillas-web/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/5-1-plantillas-web", (function(a){return {data:[{page:{slug:"5-1-plantillas-web",description:"...",title:"Plantillas Web",toc:[],body:{type:"root",children:[]},dir:"\u002F",path:"\u002F5-1-plantillas-web",extension:".md",createdAt:a,updatedAt:a}}],fetch:{},mutations:void 0}}("2022-01-24T00:49:13.509Z")));
