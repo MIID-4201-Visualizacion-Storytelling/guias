b98bde91797ff57e95d14547c4cf4b79b2ee171f
1cgonza <1cgonza@users.noreply.github.com>
2022-01-27 14:46:32 +0000

    deploy: af6a00ae7cb5c8f12a181453ce3fa1ccd0092c87

--- _nuxt/static/1643294553/2-6-ensamblaje/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fguias\u002F_nuxt\u002Fstatic\u002F1643294553",layout:"default",error:a,serverRendered:true,routePath:"\u002F2-6-ensamblaje",config:{_app:{basePath:"\u002Fguias\u002F",assetsPath:"\u002Fguias\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"d5b828ba"}}}}(null));
