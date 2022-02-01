01e7b291e50b69a39bf4bbf19eaa2946b49bc844
anattolia <anattolia@users.noreply.github.com>
2022-02-01 13:10:00 +0000

    deploy: 77c905523daed81c0feb8dffed9ff8578711839f

--- _nuxt/static/1643720849/5-4-recomendaciones/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fguias\u002F_nuxt\u002Fstatic\u002F1643720849",layout:"default",error:a,serverRendered:true,routePath:"\u002F5-4-recomendaciones",config:{_app:{basePath:"\u002Fguias\u002F",assetsPath:"\u002Fguias\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"c3829de1"}}}}(null));
