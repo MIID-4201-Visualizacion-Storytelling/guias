4b417e1bb3ef98186be7dac5bae1256013d83404
anattolia <anattolia@users.noreply.github.com>
2022-02-07 01:27:52 +0000

    deploy: a7f0ba58313af1a509627dc02e29070149f0301d

---
+++ _nuxt/static/1644197269/5-4-recomendaciones/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/5-4-recomendaciones", (function(a){return {data:[{page:{slug:"5-4-recomendaciones",description:"...",title:"Recomendaciones",toc:[],body:{type:"root",children:[]},dir:"\u002F",path:"\u002F5-4-recomendaciones",extension:".md",createdAt:a,updatedAt:a}}],fetch:{},mutations:void 0}}("2022-02-07T01:27:01.173Z")));
