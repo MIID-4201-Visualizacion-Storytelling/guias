9e47a8ac6ca5c4b9017f4645e99811cc069a233a
anattolia <anattolia@users.noreply.github.com>
2022-02-08 02:24:07 +0000

    deploy: 7eefb4f372634ab37d2cbec38d3521b33ef0eb9a

---
+++ _nuxt/static/1644287044/4-2-herramienta-propia/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/4-2-herramienta-propia", (function(a){return {data:[{page:{slug:"4-2-herramienta-propia",description:"...",title:"Herramienta Propia",toc:[],body:{type:"root",children:[]},dir:"\u002F",path:"\u002F4-2-herramienta-propia",extension:".md",createdAt:a,updatedAt:a}}],fetch:{},mutations:void 0}}("2022-02-08T02:23:08.397Z")));
