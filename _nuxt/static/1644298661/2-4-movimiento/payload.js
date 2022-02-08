2de63dadeb655590ea813e7953277541dade7f94
1cgonza <1cgonza@users.noreply.github.com>
2022-02-08 05:43:58 +0000

    deploy: 4a8120c7dea91f4a8b15e2410d7ef8071abbc29f

--- _nuxt/static/1644298661/2-4-movimiento/payload.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("/2-4-movimiento", (function(a){return {data:[{page:{slug:"2-4-movimiento",description:"...",title:"Movimiento",toc:[],body:{type:"root",children:[]},dir:"\u002F",path:"\u002F2-4-movimiento",extension:".md",createdAt:a,updatedAt:a}}],fetch:{},mutations:void 0}}("2022-02-08T05:36:45.279Z")));
--- _nuxt/static/1644298661/2-5-interaccion/payload.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("/2-5-interaccion", (function(a){return {data:[{page:{slug:"2-5-interaccion",description:"...",title:"Interacción",toc:[],body:{type:"root",children:[]},dir:"\u002F",path:"\u002F2-5-interaccion",extension:".md",createdAt:a,updatedAt:a}}],fetch:{},mutations:void 0}}("2022-02-08T05:36:45.279Z")));
