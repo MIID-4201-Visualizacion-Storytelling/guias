f3af20df09fd6dbcbbf90640f65a2530bba82c3f
anattolia <anattolia@users.noreply.github.com>
2022-02-13 00:18:06 +0000

    deploy: 3e4be8ff324abb7dae1b157d1f2fb702880092ba

--- _nuxt/static/1644711472/1-4-herramientas-www/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fguias\u002F_nuxt\u002Fstatic\u002F1644711472",layout:"default",error:a,serverRendered:true,routePath:"\u002F1-4-herramientas-www",config:{_app:{basePath:"\u002Fguias\u002F",assetsPath:"\u002Fguias\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"774c8d28"}}}}(null));
---
+++ _nuxt/static/1644711483/1-4-herramientas-www/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fguias\u002F_nuxt\u002Fstatic\u002F1644711483",layout:"default",error:a,serverRendered:true,routePath:"\u002F1-4-herramientas-www",config:{_app:{basePath:"\u002Fguias\u002F",assetsPath:"\u002Fguias\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"89beb03e"}}}}(null));
