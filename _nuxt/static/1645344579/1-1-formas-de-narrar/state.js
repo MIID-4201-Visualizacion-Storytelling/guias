a6d0d128eb165ca493d3107a617d599dba2e5738
1cgonza <1cgonza@users.noreply.github.com>
2022-02-20 09:17:17 +0000

    deploy: f17f9eba887f943db24c57e3b185d744ddfc4353

--- _nuxt/static/1645344579/1-1-formas-de-narrar/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fguias\u002F_nuxt\u002Fstatic\u002F1645344579",layout:"default",error:a,serverRendered:true,routePath:"\u002F1-1-formas-de-narrar",config:{_app:{basePath:"\u002Fguias\u002F",assetsPath:"\u002Fguias\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"610db03c"}}}}(null));
