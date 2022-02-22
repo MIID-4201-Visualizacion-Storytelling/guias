342b1ff28f4b6a55af2ce6697baedd8fc463695b
1cgonza <1cgonza@users.noreply.github.com>
2022-02-22 13:59:12 +0000

    deploy: f44b03d3cc9e20a81ad458614134e98d59b265cc

--- _nuxt/static/1645348633/3-3-visualizar-refinar/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fguias\u002F_nuxt\u002Fstatic\u002F1645348633",layout:"default",error:a,serverRendered:true,routePath:"\u002F3-3-visualizar-refinar",config:{_app:{basePath:"\u002Fguias\u002F",assetsPath:"\u002Fguias\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"2bdb60c5"}}}}(null));
---
+++ _nuxt/static/1645538348/1-3-herramientas/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fguias\u002F_nuxt\u002Fstatic\u002F1645538348",layout:"default",error:a,serverRendered:true,routePath:"\u002F1-3-herramientas",config:{_app:{basePath:"\u002Fguias\u002F",assetsPath:"\u002Fguias\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"8d51e6f4"}}}}(null));
