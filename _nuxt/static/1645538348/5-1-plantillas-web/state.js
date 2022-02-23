f60b3e79ea0addf64408c5ae0058322827e929af
anattolia <anattolia@users.noreply.github.com>
2022-02-23 16:22:14 +0000

    deploy: 8313f80ed0457b03af3cdf52ac8c03c46519cbd1

--- _nuxt/static/1645538348/5-1-plantillas-web/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fguias\u002F_nuxt\u002Fstatic\u002F1645538348",layout:"default",error:a,serverRendered:true,routePath:"\u002F5-1-plantillas-web",config:{_app:{basePath:"\u002Fguias\u002F",assetsPath:"\u002Fguias\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"8d51e6f4"}}}}(null));
