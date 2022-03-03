a90d8f9ea4741a42879386426650473ac346e819
1cgonza <1cgonza@users.noreply.github.com>
2022-03-03 23:29:04 +0000

    deploy: dba94042db7aae86f8d69074db387bc7bea04415

--- _nuxt/static/1646349198/7-1-recursos/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fguias\u002F_nuxt\u002Fstatic\u002F1646349198",layout:"default",error:a,serverRendered:true,routePath:"\u002F7-1-recursos",config:{_app:{basePath:"\u002Fguias\u002F",assetsPath:"\u002Fguias\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"e2787038"}}}}(null));
---
+++ _nuxt/static/1646350140/6-2-galeria/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/6-2-galeria", (function(a){return {data:[{page:{slug:"6-2-galeria",description:"...",title:"Galería de Proyectos",toc:[],body:{type:"root",children:[]},dir:"\u002F",path:"\u002F6-2-galeria",extension:".md",createdAt:a,updatedAt:a}}],fetch:{},mutations:void 0}}("2022-03-03T23:28:20.232Z")));
