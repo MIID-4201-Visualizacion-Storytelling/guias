844b78d123c0fc13c04081db97e0341e3e2dc3e0
anattolia <anattolia@users.noreply.github.com>
2022-03-11 20:13:17 +0000

    deploy: b6017cefe214d8e789300850df939acf647a2850

---
+++ _nuxt/static/1647029592/2-2-escalas/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fguias\u002F_nuxt\u002Fstatic\u002F1647029592",layout:"default",error:a,serverRendered:true,routePath:"\u002F2-2-escalas",config:{_app:{basePath:"\u002Fguias\u002F",assetsPath:"\u002Fguias\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"1a14f1cc"}}}}(null));
