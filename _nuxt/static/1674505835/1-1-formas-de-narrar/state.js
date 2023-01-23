015c2fa4ea8b7c41502e420c8bd2c109816dd36b
anattolia <anattolia@users.noreply.github.com>
2023-01-23 22:28:50 +0000

    deploy: d9412352a02fb1425a490524b98c777ab81c5b62

--- _nuxt/static/1674505835/1-1-formas-de-narrar/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fguias\u002F_nuxt\u002Fstatic\u002F1674505835",layout:"default",error:a,serverRendered:true,routePath:"\u002F1-1-formas-de-narrar",config:{_app:{basePath:"\u002Fguias\u002F",assetsPath:"\u002Fguias\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"e9bc3e0b"}}}}(null));
---
+++ _nuxt/static/1674512925/1-4-herramientas-www/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fguias\u002F_nuxt\u002Fstatic\u002F1674512925",layout:"default",error:a,serverRendered:true,routePath:"\u002F1-4-herramientas-www",config:{_app:{basePath:"\u002Fguias\u002F",assetsPath:"\u002Fguias\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"354f2b0e"}}}}(null));
