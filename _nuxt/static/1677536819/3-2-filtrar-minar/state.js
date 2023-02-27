5bb3fc9bc037b2facc04bcce81d63888461653e5
Juliansintilde <Juliansintilde@users.noreply.github.com>
2023-02-27 22:29:49 +0000

    deploy: ea348a68905ce510278c679909bbbb10a2430702

--- _nuxt/static/1677536819/3-2-filtrar-minar/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fguias\u002F_nuxt\u002Fstatic\u002F1677536819",layout:"default",error:a,serverRendered:true,routePath:"\u002F3-2-filtrar-minar",config:{_app:{basePath:"\u002Fguias\u002F",assetsPath:"\u002Fguias\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"d7bed11a"}}}}(null));
