803d7dbb1ee5f243ed14cf9b6ac0e0c4b910ce4a
Juliansintilde <Juliansintilde@users.noreply.github.com>
2023-02-27 22:25:49 +0000

    deploy: b4d90495c48fbf074e904254f39e3be0d698d1d0

--- _nuxt/static/1674513067/1-2-linea-de-tiempo/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fguias\u002F_nuxt\u002Fstatic\u002F1674513067",layout:"default",error:a,serverRendered:true,routePath:"\u002F1-2-linea-de-tiempo",config:{_app:{basePath:"\u002Fguias\u002F",assetsPath:"\u002Fguias\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"2689738f"}}}}(null));
