5841186b6ef076c679980e5688bc0b9c99cc7bad
Juliansintilde <Juliansintilde@users.noreply.github.com>
2023-02-27 22:29:55 +0000

    deploy: 2cd92c4d34db348c156cb026d7752922ea7b213c

--- _nuxt/static/1677536985/6-2-galeria/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fguias\u002F_nuxt\u002Fstatic\u002F1677536985",layout:"default",error:a,serverRendered:true,routePath:"\u002F6-2-galeria",config:{_app:{basePath:"\u002Fguias\u002F",assetsPath:"\u002Fguias\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"4146d3db"}}}}(null));
---
+++ _nuxt/static/1677536991/2-3-espacializacion/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fguias\u002F_nuxt\u002Fstatic\u002F1677536991",layout:"default",error:a,serverRendered:true,routePath:"\u002F2-3-espacializacion",config:{_app:{basePath:"\u002Fguias\u002F",assetsPath:"\u002Fguias\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"4e66ff48"}}}}(null));
