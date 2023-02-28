b47d4ec065986c56cc41c444cf34c1a079f1e438
1cgonza <1cgonza@users.noreply.github.com>
2023-02-28 13:44:05 +0000

    deploy: d46f4030a4285a8977759d923f460cfc2771d1dc

---
+++ _nuxt/static/1677591841/4-1-herramientas-BI/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fguias\u002F_nuxt\u002Fstatic\u002F1677591841",layout:"default",error:a,serverRendered:true,routePath:"\u002F4-1-herramientas-BI",config:{_app:{basePath:"\u002Fguias\u002F",assetsPath:"\u002Fguias\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"282c7300"}}}}(null));
