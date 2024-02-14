5bc9016421751af71d9964baa5c4ee9b0a940631
1cgonza <1cgonza@users.noreply.github.com>
2024-02-14 16:56:09 +0000

    deploy: 9d9f1630f3c83ad4d9d572a07aea23ac87911a03

--- _nuxt/static/1707855867/2-2-escalas/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fguias\u002F_nuxt\u002Fstatic\u002F1707855867",layout:"default",error:a,serverRendered:true,routePath:"\u002F2-2-escalas",config:{_app:{basePath:"\u002Fguias\u002F",assetsPath:"\u002Fguias\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"9aa7e730"}}}}(null));
---
+++ _nuxt/static/1707929765/5-2-elementos-descriptivos/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fguias\u002F_nuxt\u002Fstatic\u002F1707929765",layout:"default",error:a,serverRendered:true,routePath:"\u002F5-2-elementos-descriptivos",config:{_app:{basePath:"\u002Fguias\u002F",assetsPath:"\u002Fguias\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"540784c9"}}}}(null));
