5495bc6d2e2e0046858375bacc94b6741cec0811
1cgonza <1cgonza@users.noreply.github.com>
2024-02-23 16:06:57 +0000

    deploy: a75ce3153c3c2bf6d2e27cfe90e92cedfa314ec4

--- _nuxt/static/1708552317/7-1-recursos/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fguias\u002F_nuxt\u002Fstatic\u002F1708552317",layout:"default",error:a,serverRendered:true,routePath:"\u002F7-1-recursos",config:{_app:{basePath:"\u002Fguias\u002F",assetsPath:"\u002Fguias\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"48bba9ca"}}}}(null));
